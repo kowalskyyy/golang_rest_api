302bfbcceac24a9b5517f7d7e7801c191a52bf46
Bartosz Kowalski <kowbar23@gmail.com>
2024-05-23 10:42:30 +0200

    add get items by customer id api

--- main.go
+++ main.go
@@ -8,35 +8,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type items struct {
+type item struct {
 	ItemID  string  `json:"itemId"`
 	CostEur float32 `json:"costEur"`
 }
 
-type order struct {
-	CustomerID string  `json:"customerId"`
-	OrderID    string  `json:"orderId"`
-	Timestamp  string  `json:"timestamp"`
-	Items      []items `json:"items"`
+type customerItem struct {
+	item
+	CustomerID string `json:"customerId"`
 }
 
-var orders = []order{
-	{CustomerID: "01",
-		OrderID:   "50",
-		Timestamp: "1637245070513",
-		Items: []items{{
-			ItemID: "20201", CostEur: 2},
-		},
-	},
-	{CustomerID: "01",
-		OrderID:   "50",
-		Timestamp: "1637245070513",
-		Items: []items{{
-			ItemID: "20201", CostEur: 2},
-		},
-	}}
+type order struct {
+	CustomerID string `json:"customerId"`
+	OrderID    string `json:"orderId"`
+	Timestamp  string `json:"timestamp"`
+	Items      []item `json:"items"`
+}
+
+var orders = []order{}
 
 func getOrders(context *gin.Context) {
+	if len(orders) == 0 {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "There are no orders in the system. Please submit orders first."})
+		return
+	}
 	context.IndentedJSON(http.StatusOK, orders)
 }
 
@@ -51,10 +46,40 @@ func submitOrders(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, newOrders)
 }
 
+func getItemsByCustomer(context *gin.Context) {
+	id := context.Param("id")
+	var result []customerItem
+	if len(orders) != 0 {
+		for _, order := range orders {
+			if order.CustomerID == id {
+				for _, item_ := range order.Items {
+					extended := customerItem{
+						item: item{
+							ItemID:  item_.ItemID,
+							CostEur: item_.CostEur,
+						},
+						CustomerID: id,
+					}
+					result = append(result, extended)
+				}
+			}
+		}
+		if len(result) != 0 {
+			context.IndentedJSON(http.StatusOK, result)
+			return
+		} else {
+			context.IndentedJSON(http.StatusNotFound, gin.H{"message": "There are no orders associated with this customer ID"})
+			return
+		}
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "There are no orders in the system. Please submit orders first."})
+}
+
 func main() {
 	fmt.Printf("elo mordo")
 	router := gin.Default()
 	router.GET("/get-orders", getOrders)
+	router.GET("/get-items/:id", getItemsByCustomer)
 	router.POST("/submit-orders", submitOrders)
 	router.Run("localhost:8080")
 }
